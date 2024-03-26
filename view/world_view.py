a86c8829cbaabc51ade37fd70ef657d0cca99880
Rani <rani.baghezza@gmail.com>
2024-03-26 15:28:38 -0400

    added goblin to world view

--- view/world_view.py
+++ view/world_view.py
@@ -2,17 +2,25 @@ import sys
 import pygame
 from view import view_constants as view_cst
 
+#TODO: set location where the character is on the map
 class WorldView:
     def __init__(self, screen):
         self.screen = screen
+
         self.character_image = pygame.image.load("assets/sprites/character.png").convert_alpha()
         self.character_rect = self.character_image.get_rect(center=(view_cst.WIDTH // 2, view_cst.HEIGHT // 2))
+
+        self.goblin_image = pygame.image.load("assets/sprites/goblin.png").convert_alpha()
+        self.goblin_rect = self.goblin_image.get_rect(center=(view_cst.WIDTH // 4, view_cst.HEIGHT // 4))
+
+
         self.back_button_text = pygame.font.SysFont("Arial", 20).render("Back", True, view_cst.TEXT_COLOR)
         self.back_button_rect = self.back_button_text.get_rect(topright=(view_cst.WIDTH - 10, 10))
 
     def display_world(self, x, y):
         self.screen.fill(view_cst.WHITE)
         self.screen.blit(self.character_image, self.character_rect)
+        self.screen.blit(self.goblin_image, self.goblin_rect)
         self.screen.blit(self.back_button_text, self.back_button_rect)
         self.display_coordinates(x, y)
         pygame.display.flip()
